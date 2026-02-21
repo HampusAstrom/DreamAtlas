41baabb78c6d95abdcfbd36d66c513bfb933daa8
Chuzzle <hmt.autio@gmail.com>
2026-02-21 23:25:12 +0100

    Initial drafts for some map class, settings and graph generation.

---
+++ FlowAtlas_generator/flow_map.py
@@ -0,0 +1,15 @@
+from dreamAtlas.classes import DominionsMap as DAmap
+from flow_types import FlowSettings
+
+class FlowMap(DAmap):
+    def __init__(self, settings : FlowSettings):
+        DAmap.__init__(self)
+        self.settings = settings
+        self.num_regions = settings.num_players + int(0.5 * settings.cap_connections * settings.num_players)
+        self.num_prov = settings.num_players * settings.num_prov_per_player
+        self.num_prov_per_region = int(self.num_prov / self.num_regions) if self.num_regions > 0 else 0
+        self.map_size = (settings.map_size_x, settings.map_size_y)
+
+def setup(mapObject : FlowMap):
+    #Load any necessary resources, set up objects and constants
+    pass
