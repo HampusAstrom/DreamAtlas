823cf9aa8bfeee23b1ac767fdfc422748aa7c5d4
Hampus Åström <hampus.thep@gmail.com>
2026-02-23 00:05:06 +0100

    Commented out calling non-extiting classes

--- flow_atlas/map_generator.py
+++ flow_atlas/map_generator.py
@@ -5,13 +5,13 @@ def run():
     settings = FlowSettings()
     mapObject = FlowMap(settings)
     setup(mapObject) #Set up objects and constants, load any necessary resources
-    generate_nodes(mapObject) #Place province capitals
-    generate_connections(mapObject)
-    generate_provinces(mapObject) #Generate province borders, determine possible start locations/throne sites ?
-    generate_border_terrain(mapObject)
-    generate_province_terrain(mapObject) #use wavefunction collapse to determine terrain for all provinces
-    finalise_graph(mapObject) #I suspect this will be needed, but not really sure what should happen here yet - probably balancing
-    place_special_sites(mapObject)
-    generate_graphics(mapObject)
-    export_map(mapObject)
+    # generate_nodes(mapObject) #Place province capitals
+    # generate_connections(mapObject)
+    # generate_provinces(mapObject) #Generate province borders, determine possible start locations/throne sites ?
+    # generate_border_terrain(mapObject)
+    # generate_province_terrain(mapObject) #use wavefunction collapse to determine terrain for all provinces
+    # finalise_graph(mapObject) #I suspect this will be needed, but not really sure what should happen here yet - probably balancing
+    # place_special_sites(mapObject)
+    # generate_graphics(mapObject)
+    # export_map(mapObject)
 #10, 15 or 20 provinces per player ish
