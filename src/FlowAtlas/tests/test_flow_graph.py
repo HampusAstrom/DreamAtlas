d42a37e20cb4039843a94bac59417ee24bf01768
Chuzzle <hmt.autio@gmail.com>
2026-03-05 23:46:01 +0100

    Fixed graph tests.

--- src/FlowAtlas/tests/test_flow_graph.py
+++ src/FlowAtlas/tests/test_flow_graph.py
@@ -1,27 +1,34 @@
-import pytest
+import unittest
 import networkx as nx
-from FlowAtlas.flow_types import MapNode
+from FlowAtlas.flow_types import ProvinceNode
 
-def test_graph_generation():
-    test_graph = nx.Graph()
-    assert(test_graph)
+class TestGraphGeneration(unittest.TestCase):
 
-    node1 = MapNode()
-    test_graph.add_node(node1)
+    def test_graph_generation(self):
+        test_graph = nx.Graph()
+        self.assertIsNotNone(test_graph)
 
-    node_list = [MapNode() for _ in range(10)]
-    test_graph.add_nodes_from(node_list)
+        node1 = ProvinceNode()
+        test_graph.add_node(node1)
 
-    test_graph.add_edge(node1, node_list[1])
+        node_list = [ProvinceNode() for _ in range(10)]
+        test_graph.add_nodes_from(node_list)
 
-    edge_list = [ ( node1, node_list[i]) for i in range(10)]
+        test_graph.add_edge(node1, node_list[1])
 
-    test_graph.add_edges_from(edge_list)
+        edge_list = [ ( node1, node_list[i]) for i in range(10)]
 
-    assert(test_graph.degree[node1] is 10)
-    for node in node_list:
-        assert(test_graph.degree[node] is 1)
-        assert(test_graph.adj[node] is node1)
+        test_graph.add_edges_from(edge_list)
+
+        graph_degrees = test_graph.degree
+        assert isinstance(graph_degrees, nx.classes.reportviews.DegreeView)
+        node1_degree = graph_degrees[node1]
+        self.assertIsInstance(node1_degree, int)
+        self.assertEqual(node1_degree, 10)
+        for node in node_list:
+            node_degree = graph_degrees[node]
+            self.assertEqual(node_degree, 1)
+            self.assertEqual(test_graph.adj[node], {node1 : {}})
 
 if __name__ == '__main__':
-    pytest.main([__file__, '-v'])
+    unittest.main()
